241cabbf309a3278304dea98b5c3894d9bb1165b
Matias Alvarez <matiasalvarezbustos@gmail.com>
2024-11-25 14:27:20 -0300

    hero-slider terminado (html-css-js)

--- js/js.js
+++ js/js.js
@@ -7,17 +7,16 @@ let sliderSectionLast = sliderSection[sliderSection.length -1];
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next() {
-    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
-    
     setTimeout(function() {
         slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', .sliderSectionFirst);
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
         slider.style.marginLeft = "-100%";
     }, 500);
 }
 
-btnRight.addEventListener('click', function() {
-    Next();
-});
+setInterval(function() {
+    Next()
+}, 3000)
